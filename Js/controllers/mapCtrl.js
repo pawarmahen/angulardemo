6e407b4ffbd2b0c4a1838a516224149db4a5b3f1
MrigendraD <mrigendra.dwivedi@ASEN-D-AD089.asentech.com>
2019-02-07 19:12:18 +0530

    bg color added for state

--- Js/controllers/mapCtrl.js
+++ Js/controllers/mapCtrl.js
@@ -21,6 +21,13 @@
                 CountryHoverColor: '#008cb2',
                 DefaultColor: '#cfd1d0',
                 backgroundColor: "#f2f2f2",
+				StateDefaultColor: "#cfd1d0",
+				StateColor: "#00b5e6",
+				StateHoverColor: "#008cb2",
+				CityPinColor: "#00b5e6",
+				CityPinHoverColor: "#008cb2",
+				BackgroundColor: "#f2f2f2"
+
                 countryKey:'countries/us/us-all',
                 data : ['us-tx', 'us-wv', 'us-il', 'us-nj', 'pr-3614', 'us-vt']
             };
