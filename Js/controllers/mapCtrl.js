b46ae7e7bf005ab241e57621317e5479450a492b
MrigendraD <mrigendra.dwivedi@ASEN-D-AD089.asentech.com>
2019-02-07 19:21:29 +0530

    Added comma at the end of the line and space removed

--- Js/controllers/mapCtrl.js
+++ Js/controllers/mapCtrl.js
@@ -26,8 +26,7 @@
 				StateHoverColor: "#008cb2",
 				CityPinColor: "#00b5e6",
 				CityPinHoverColor: "#008cb2",
-				BackgroundColor: "#f2f2f2"
-
+				BackgroundColor: "#f2f2f2",
                 countryKey:'countries/us/us-all',
                 data : ['us-tx', 'us-wv', 'us-il', 'us-nj', 'pr-3614', 'us-vt']
             };
